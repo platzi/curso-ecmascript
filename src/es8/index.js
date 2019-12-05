26a76e07ceea84090fb311f2c3a7150321b3010e
Oscar Barajas <oscar@dragons.mx>
2019-12-05 10:40:39 -0500

    Here be Dragons

---
+++ src/es8/index.js
@@ -0,0 +1,29 @@
+const data = {
+  frontend: 'Oscar',
+  backend: 'ISabel',
+  design: 'Ana',
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+const data = {
+  frontend: 'Oscar',
+  backend: 'ISabel',
+  design: 'Ana',
+}
+
+const values = Object.values(data);
+console.log(values)
+console.log(values.length)
+
+
+const string = 'hello';
+console.log(string.padStart(7,'hi'));
+console.log(string.padEnd(12, ' -----'))
+console.log('food'.padEnd(12, '  -----'))
+
+const obj = {
+  name: 'oscar',
+}

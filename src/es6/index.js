dc2c7662900331ea16cee0feb419c96179e93995
Oscar Barajas <oscar@dragons.mx>
2019-12-05 10:08:32 -0500

    This is where it all begins...

--- src/es6/index.js
+++ src/es6/index.js
@@ -107,3 +107,36 @@ const helloPromise = () => {
 helloPromise()
   .then(response => console.log(response))
   .catch(error => console.log(error));
+
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
+
+import { hello } from './module';
+
+hello();
+
+function* helloWorld() {
+  if (true) {
+    yield 'Hello, ';
+  }
+  if (true) {
+    yield 'World';
+  }
+};
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);

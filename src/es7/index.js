ce6dbf9cf4c3453dcb7bc08233a3780a00e21311
Oscar Barajas <oscar@dragons.mx>
2019-12-05 10:22:29 -0500

    Batman! (this commit has no parents)

---
+++ src/es7/index.js
@@ -0,0 +1,13 @@
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(9)) {
+  console.log('Si se encuentra el valor 7');
+} else {
+  console.log('No se encuentra.')
+}
+
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);

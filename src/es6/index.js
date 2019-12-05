5eb0b8cdebc579a31b5d610ff839fcdec070c48e
Oscar Barajas <oscar@dragons.mx>
2019-12-05 09:30:51 -0500

    I'll explain when you're older!

--- src/es6/index.js
+++ src/es6/index.js
@@ -19,3 +19,47 @@ let epicPhrase = hello + ' ' + world;
 console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+let lorem = "Qui consequatur. Commodi. Ipsum vel duis yet minima \n"
+  + "otra frase epica que necesitamos."
+
+// es6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+  'name': 'oscar',
+  'age': 32,
+  'country': 'MX'
+}
+
+console.log(person.name, person.age);
+
+let { name } = person;
+console.log(name);
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+let education = ['David', ...team1, ...team2];
+
+console.log(education);
+
+{
+  var globalVar = "Global Var";
+}
+
+{
+  let globalLet = 'Global Let';
+  console.log(globalLet);
+}
+
+console.log(globalVar);
+
+const a = 'b';
+a = 'a';
+console.log(a);

03d68631e0f578114f2009ac33b42757b13e0632
Oscar Barajas Tavares <oscar@arepa.dev>
2019-12-13 15:31:25 -0500

    This commit is a lie

--- src/es8/index.js
+++ src/es8/index.js
@@ -20,10 +20,36 @@ console.log(values.length)
 
 
 const string = 'hello';
-console.log(string.padStart(7,'hi'));
+console.log(string.padStart(7, 'hi'));
 console.log(string.padEnd(12, ' -----'))
 console.log('food'.padEnd(12, '  -----'))
 
 const obj = {
   name: 'oscar',
 }
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    (false)
+      ? setTimeout(() => resolve('Hello World'), 3000)
+      : reject(new Error('Test Error'))
+  })
+};
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+anotherFunction();
